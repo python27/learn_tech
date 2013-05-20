577b62934558477cce51c78a7e8b7a29007ada7a
python27 <silfer.lee@gmail.com>
2013-05-20 20:26:43 +0800

    use platform exp&log func

---
+++ learn_cmake/step4/MathFunctions/mysqrt.cxx
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include"MathFunctions.h"
+#include"TutorialConfig.h"
+
+#include<math.h>
+
+double mysqrt(double x)
+{
+    if (x <= 0)
+    {
+        return 0;
+    }
+
+    double result;
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    result = exp(log(x)*0.5);
+    fprintf(stdout, "Computing sqrt of %g to be %g using log\n",x, result);
+#else
+    double delta;
+    result = x;
+
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        if (result <= 0)
+        {
+            result = 0.1;
+        }
+
+        delta = x - (result * result);
+        result += 0.5 * delta / result;
+        fprintf(stdout, "Computing sqrt of %g to be %g\n",x, result);
+    }
+#endif
+    return result;
+}
