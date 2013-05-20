884966accea752b65c0aebe1ac45539c88337200
python27 <silfer.lee@gmail.com>
2013-05-20 16:45:02 +0800

    cmake tutorial step1 and step2

---
+++ learn_cmake/step2/MathFunctions/mysqrt.cxx
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"MathFunctions.h"
+
+double mysqrt(double x)
+{
+    if (x <= 0)
+    {
+        return 0;
+    }
+
+    double result;
+    double delta;
+    result = x;
+
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        if (result <= 0)
+        {
+            result = 0.1;
+        }
+
+        delta = x - (result * result);
+        result += 0.5 * delta / result;
+        fprintf(stdout, "Computing sqrt of %g to be %g\n",x, result);
+    }
+    return result;
+}
