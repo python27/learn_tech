d9bcda89c2a340f5f11848280b3eff247029ae9b
python27 <silfer.lee@gmail.com>
2013-06-02 20:46:51 +0800

    "finished but not debug"

--- learn_pyqt4/the_tetris_game.py
+++ learn_pyqt4/the_tetris_game.py
@@ -1,5 +1,5 @@
 #!usr/bin/python
-#
+#-*- coding: utf-8 -*-
 
 
 import sys
@@ -17,6 +17,7 @@ class Tetris(QtGui.QMainWindow):
         self.tetrisboard = Board(self)
 
         self.setCentralWidget(self.tetrisboard)
+        self.statusbar = self.statusBar()
         self.connect(self.tetrisboard, QtCore.SIGNAL(
             "messageToStatusbar(QString)"),
                 self.statusbar, QtCore.SLOT("showMessage(QString)"))
@@ -99,7 +100,162 @@ class Board(QtGui.QFrame):
         self.update()
 
     def paintEvent(self, event):
-        pass
+        painter = QtGui.QPainter(self)
+        rect = self.contentsRect()
+
+        boardTop = rect.bottom() - Board.BoardHeight * self.squareHeight()
+
+        for i in range(Board.BoardHeight):
+            for j in range(Board.BoardWidth):
+                shape = self.shapeAt(j, Board.BoardHeight - i - 1)
+                if shape != Tetrominoes.NoShape:
+                    self.drawSquare(painter,
+                        rect.left() + j * self.squareWidth(),
+                        boardTop + i * self.squareHeight(), shape)
+
+        if self.curPiece.shape() != Tetrominoes.NoShape:
+            for i in range(4):
+                x = self.curX + self.curPiece.x(i)
+                y = self.curY - self.curPiece.y(i)
+                self.drawSquare(painter,
+                    rect.left() + x * self.squareWidth(),
+                    boardTop + (Board.BoardHeight - y - 1) * self.squareHeight(),
+                    self.curPiece.shape())
+
+    def KeyPressEvent(self, event):
+        if not self.isStarted or self.curPiece.shape() == Tetrominoes.NoShape:
+            QtGui.QWidget.KeyPressEvent(self, event)
+            return
+
+        key = event.key()
+        if key == QtCore.Qt.Key_P:
+            self.pause()
+            return
+        if self.isPaused:
+            return
+        elif key == QtCore.Qt.Key_Left:
+            self.tryMove(self.curPiece, self.curX - 1, self.curY)
+        elif key == QtCore.Qt.Key_Right:
+            self.tryMove(self.curPiece, self.curX + 1, self.curY)
+        elif key == QtCore.Qt.Key_Down:
+            self.tryMove(self.curPiece.rotatedRight(), self.curX, self.curY)
+        elif key == QtCore.Qt.Key_Up:
+            self.tryMove(self.curPiece.rotatedLeft(), self.curX, self.curY)
+        elif key == QtCore.Qt.Key_Space:
+            self.dropDown()
+        elif key == QtCore.Qt.Key_D:
+            self.oneLineDown()
+        else:
+            QtGui.Widget.keyPressEvent(self, event)
+
+    def timerEvent(self, event):
+        if event.timerId() == self.timer.timerId():
+            if self.isWaitingAferLine:
+                self.isWaitingAferLine = False
+                self.newPiece()
+            else:
+                self.oneLineDown()
+        else:
+            QtGui.QFrame.timerEvent(self, event)
+
+    def clearBoard(self):
+        for i in range(Board.BoardHeight * Board.BoardWidth):
+            self.board.append(Tetrominoes.NoShape)
+
+    def dropDown(self):
+        newY = self.curY
+        while newY > 0:
+            if not self.tryMove(self.curPiece, self.curX, newY - 1):
+                break
+            newY = -1
+
+    def oneLineDown(self):
+        if not self.tryMove(self.curPiece, self.curX, self.curY - 1):
+            self.pieceDropped()
+
+    def pieceDropped(self):
+        for i in range(4):
+            x = self.curX + self.curPiece.x(i)
+            y = self.curY - self.curPiece.y(i)
+            self.setShapeAt(x, y, self.curPiece.shape())
+
+        self.removeFullLines()
+
+        if not self.isWaitingAferLine:
+            self.newPiece()
+
+    def removeFullLines(self):
+        numFullLines = 0
+        rowsToRemove = []
+        for i in range(Board.BoardHeight):
+            n = 0
+            for j in range(Board.BoardWidth):
+                if not self.shapeAt(j, i) == Tetrominoes.NoShape:
+                    n = n + 1
+            if n == 10:
+                rowsToRemove.append(i)
+
+        rowsToRemove.reverse()
+
+        for m in rowsToRemove:
+            for k in range(m, Board.BoardHeight):
+                for l in range(Board.BoardWidth):
+                    self.setShapeAt(l, k, self.shapeAt(l, k + 1))
+
+        numFullLines = numFullLines + len(rowsToRemove)
+
+        if numFullLines > 0:
+            self.numLinesRemoved = self.numLinesRemoved + numFullLines
+            self.emit(QtCore.SIGNAL("messageToStatusbar(QString)"),
+            str(self.numLinesRemoved))
+            self.isWaitingAferLine = True
+            self.curPiece.setShape(Tetrominoes.NoShape)
+            self.update()
+
+    def newPiece(self):
+        self.curPiece = self.nextPiece
+        self.nextPiece.setRandomShape()
+        self.curX = Board.BoardWidth / 2 + 1
+        self.curY = Board.BoardHeight - 1 + self.curPiece.minY()
+
+        if not self.tryMove(self.curPiece, self.curX, self.curY):
+            self.curPiece.setShape(Tetrominoes.NoShape)
+            self.timer.stop
+            self.isStarted = False
+            self.emit(QtCore.SIGNAL("messageToStatusbar(QString)"), "Game over")
+
+    def tryMove(self, newPiece, newX, newY):
+        for i in range(4):
+            x = newX + newPiece.x(i)
+            y = newY - newPiece.y(i)
+            if x < 0 or x >= Board.BoardWidth or y < 0 or y >= Board.BoardHeight:
+                return False
+            if self.shapeAt(x, y) != Tetrominoes.NoShape:
+                return False
+
+        self.curPiece = newPiece
+        self.curX = newX
+        self.curY = newY
+        self.update()
+        return True
+
+    def drawSquare(self, painter, x, y, shape):
+        colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
+            0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
+
+        color = QtGui.QColor(colorTable[shape])
+        painter.fillRect(x + 1, y + 1, self.squareWidth() - 2,
+            self.squareHeight() - 2, color)
+
+        painter.setPen(color.light())
+        painter.drawLine(x, y + self.squareHeight() - 1, x, y)
+        painter.drawLine(x, y, x + self.squareWidth() - 1, y)
+
+        painter.setPen(color.dark())
+        painter.drawLine(x + 1, y + self.squareHeight() - 1,
+            x + self.squareWidth() - 1, y + self.squareHeight() - 1)
+        painter.drawLine(x + self.squareWidth() - 1, y + self.squareHeight() - 1,
+            x + self.squareWidth() - 1, y + 1)
 
 
 class Tetrominoes(object):
